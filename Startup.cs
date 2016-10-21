ffe6ae815105d28d6bcbe1fb5d7e8809ff690d38
João Carlos Macedo Flores dos Santos <joaofloressantos@gmail.com>
2016-10-21 09:55:41 +0100

    Getting started.

---
+++ Startup.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
